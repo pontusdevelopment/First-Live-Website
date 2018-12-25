662911a3d2e7a933ab4f494e3cea94d703201d4d
Webdev - no distractions <webdev@Pontuss-iMac.local>
2018-12-25 22:41:03 +0100

    Initial commit, html, css, and JS

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("Alright, if you checked the developer console, you will also find this.");
+console.log("Although, this does render before the HTML due to the positon of the script in the HTML-file");
+console.log("So technically, when I write this is literally it in the paragraph, I wasn't lying, because this had already rendered");
+console.log("Oh, and also I'm too lazy to do newlines, so I just repeated console.logs, deal with it.")
